4f5b9b3769095cf160464f866377a07ce6dba872
Ahmed Alhayek <ahmadalhayek210@gmail.com>
2026-08-24 17:41:41 +0300

    globinsights

--- src/Videos/config.ts
+++ src/Videos/config.ts
@@ -69,6 +69,7 @@ import Dossiers51Template from "./dossiers51";
 import TounesContreTemplate from "./tounescontre";
 import TruthfrompalestineTemplate from "./truthfrompalestine";
 import TheredstripesTemplate from "./theredstripes";
+import GlobinsightsTemplate from "./globinsights";
 // import Arabi21LightTemplate from "./arabi21light";
 // import Arabi21AsdaaTemplate from "./arabi21asdaa";
 // import Arabi21SportTemplate from "./arabi21sport";
@@ -87,6 +88,48 @@ export interface VideoTemplateConfig {
 }
 // TurkpressTemplate TheredstripesTemplate
 export const VIDEO_TEMPLATES: VideoTemplateConfig[] = [
+  {
+    id: "globinsights",
+    component: GlobinsightsTemplate,
+    width: 1080,
+    height: 1920,
+    outroUrl: staticFile("short/globinsights/images/outro.webm"),
+    defaultVideoSrc: "",
+    outroOverlapFrames: 0,
+    defaultData: {
+      sequences: [
+        {
+          start: 0,
+          end: 20,
+          crop: { top_left: [0, 0], bottom_right: [0, 0] },
+          volume: 1,
+          videoSrc: staticFile("Sequence05_1.mp4"),
+          blur: [],
+        },
+      ],
+      title: {
+        text: "التي انطلقت لنصرة ال قدس ال قدس والأق صى",
+      },
+      captions: {
+        src: staticFile("subtitle_translated_4360.srt"),
+      },
+      tags: {
+        location: "إسطنبول",
+        date: "20-20-2000",
+        source: "مادي بلوك",
+      },
+      speakers: [
+        {
+          start: 6,
+          name: "بيب غوارديولا",
+          description: "مدرب مانشستر سيتي",
+        },
+      ],
+      private_source: false,
+      scale_to_fit: false,
+      background_img_url: "",
+    },
+  },
   {
     id: "theredstripes",
     component: TheredstripesTemplate,
